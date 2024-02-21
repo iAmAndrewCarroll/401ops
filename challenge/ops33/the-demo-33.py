f78cf7a2cf60316e2ead6ba64a59545c0361b209
Andrew Thomas Carroll <athomascarroll@gmail.com>
2024-02-21 14:03:44 -0800

    ops 33

--- challenge/ops33/the-demo-33.py
+++ challenge/ops33/the-demo-33.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+
 # The below demo script works in tandem with virustotal-search.py from https://github.com/eduardxyz/virustotal-search, which must be in the same directory.
 # Set your environment variable first to keep it out of your script here.
 
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+print("Loaded API Key: ", os.getenv('API_KEY_VIRUSTOTAL'))
+
 apikey = os.getenv('API_KEY_VIRUSTOTAL') # Set your environment variable before proceeding. You'll need a free API key from virustotal.com so get signed up there first.
 hash = 'D41D8CD98F00B204E9800998ECF8427E' # Set your hash here. 
 
+print("API Key: ", apikey)
+
 # This concatenates everything into a working shell statement that gets passed into virustotal-search.py
-query = 'python3 virustotal-search.py -k ' + apikey + ' -m ' + hash
+query = 'python3 virustotal-search.py -k ' + str(apikey) + ' -m ' + str(hash)
+
+print("Executing Command: ", query)
 
 os.system(query)
